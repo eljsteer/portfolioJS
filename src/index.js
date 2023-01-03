d6ce615aa020a38a19eae5de69a2dd4993a22b36
Jason <el.jsteer@gmail.com>
2023-01-03 17:31:02 +1100

    updated paths for portfolio

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import "./styles/normalize.css";
-import "./styles/index.css";
+import "./Styles/normalize.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
